5a7f5cd8606d1d812f52c4d63b2f4201847fa9ca
Tony <toekneegaming123@gmail.com>
2025-07-20 15:35:32 -0400

    v1.0.0

---
+++ PlayerOptionsPatch.cs
@@ -0,0 +1,14 @@
+﻿using HarmonyLib;
+
+namespace RemoveUIEffects;
+
+[HarmonyPatch(typeof(PlayerOptions))]
+public class PlayerOptionsPatch
+{
+    [HarmonyPrefix]
+    [HarmonyPatch("SetUIDistortion")]
+    static void SetUIDistortion_Prefix(ref float value) => value = 0;
+    [HarmonyPrefix]
+    [HarmonyPatch("SetUIBloom")]
+    static void SetUIBloom_Prefix(ref float value) => value = 0;
+}
---
+++ Plugin.cs
@@ -0,0 +1,21 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+
+namespace RemoveUIEffects;
+
+[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+public class Plugin : BaseUnityPlugin
+{
+    internal static new ManualLogSource Logger;
+    private Harmony _harmony = new Harmony(MyPluginInfo.PLUGIN_GUID);
+        
+    private void Awake()
+    {
+        Logger = base.Logger;
+
+        _harmony.PatchAll(typeof(PlayerOptionsPatch));
+
+        Logger.LogInfo($"{MyPluginInfo.PLUGIN_GUID} loaded!");
+    }
+}
